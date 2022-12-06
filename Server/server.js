3b359481eb9ec8a8340eaf54a077bcad9a042692
JeremiahClifford <63067769+JeremiahClifford@users.noreply.github.com>
2022-12-06 18:33:18 -0500

    Server Refactor
    -Refactored the manager page into an html file which is loaded rather than text which is written in the server.js file

--- Server/server.js
+++ Server/server.js
@@ -9,29 +9,15 @@ const TestFunction = () => {
     return "Data sent and received"
 }
 
+app.get("/", (request, response) => {
+    response.send("Server Page")
+})
+
 //game manager page to manage the game settings
-//html setup
-const ManagerHTML = () => {
-    let managerHTML = ""
-    managerHTML += "<!DOCTYPE html>"
-    managerHTML += "<html lang='en'>"
-    managerHTML +=     "<head>"
-    managerHTML +=         "<meta charset='UTF-8'>"
-    managerHTML +=         "<meta http-equiv='X-UA-Compatible' content='IE=edge'>"
-    managerHTML +=         "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
-    managerHTML +=         "<title>Game Manager</title>"
-    managerHTML +=     "</head>"
-    managerHTML +=     "<body>"
-    managerHTML +=         "<h2>Game Manager</h2>"
-    managerHTML +=     "</body>"
-    managerHTML += "</html>"
-    return managerHTML
-}
-//page setup
 app.get("/manager", (request, response) => {
-    response.send(ManagerHTML())
-});
+    response.sendFile(__dirname + "/manager.html")
+})
 
 app.listen(settings.Port, () => {
     console.log("Listen on the port " + settings.Port + "...")
-});
+})
