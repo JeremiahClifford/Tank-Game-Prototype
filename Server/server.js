b78f782e3cb945b39d3647596bc9adc24c965fe0
JeremiahClifford <63067769+JeremiahClifford@users.noreply.github.com>
2022-12-06 12:14:16 -0500

    Server
    -Got a basic server working and added the Game Manager page to the server instead of the client
    -Removed the Game Manager page from the client

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,35 @@
 const express = require("express");
+const app = express();
+
+const port = 3000
+
+app.get("/", (request, response) => {
+    response.send("Hi there");
+});
+
+//game manager page to manage the game settings
+//html setup
+let managerHTML = ""
+const assignManagerHTML = () => {
+    managerHTML += "<!DOCTYPE html>"
+    managerHTML += "<html lang='en'>"
+    managerHTML +=     "<head>"
+    managerHTML +=         "<meta charset='UTF-8'>"
+    managerHTML +=         "<meta http-equiv='X-UA-Compatible' content='IE=edge'>"
+    managerHTML +=         "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+    managerHTML +=         "<title>Game Manager</title>"
+    managerHTML +=     "</head>"
+    managerHTML +=     "<body>"
+    managerHTML +=         "<h2>Game Manager</h2>"
+    managerHTML +=     "</body>"
+    managerHTML += "</html>"
+}
+assignManagerHTML();
+//page setup
+app.get("/manager", (request, response) => {
+    response.send(managerHTML);
+});
+
+app.listen(port, () => {
+    console.log("Listen on the port 3000...");
+});
