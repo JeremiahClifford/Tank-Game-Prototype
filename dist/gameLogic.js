7e4fc0f28fe5dae44bdcfab6cf70de96d4046e98
JeremiahClifford <63067769+JeremiahClifford@users.noreply.github.com>
2022-12-15 23:28:46 -0500

    Interactions and beginnings of moving
    -Context menu now has a button zone div which is where the interaction buttons go
    -Selecting another player now brings up a button to send them an action point. At the moment this button does not actually send them an action point
    -Selecting yourself now brings up a move button. pressing this button runs  the initiateMove() function which puts indicators are on the player in all of the spaces they can move to and puts the moving boolean to true so that in the space selecting function, the player can move to the square they click on. the actual moving is not yet implemented and will involve sending a move request to the server which was not yet been implemented

--- dist/gameLogic.js
+++ dist/gameLogic.js
@@ -11,8 +11,13 @@ const port = playerStorage.getItem("Port");
 const playerSize = 40;
 //defines the size of the drawn board squares
 const boardSquareSize = 50;
+//defines the size of the indicator marks
+const indicatorMarkSize = 30;
 //holds a list of the players
 let players = [];
+//variables to hold the status of the client
+//bool to hold the status of if the player is currently trying to move
+let moving = false;
 //how far apart 2 tanks are for the purpose of checking if a tank is within
 //range to shoot
 const CheckRangeBetweenTanks = (tankA, tankB) => Math.max(Math.abs(tankA.Position.xCoordinate - tankB.Position.xCoordinate), Math.abs(tankA.Position.yCoordinate - tankB.Position.yCoordinate));
@@ -30,8 +35,11 @@ const GetMouseGridPosition = (canvas, event) => {
 };
 //function to select a grid square
 const SelectGridSquare = (gridPosition) => {
-    //TODO: Detect if a player is in that position and open the appropriate
-    //context menu based on if it is their tank or another player's
+    //handles if the player is clicking on a space to try to move to it
+    if (moving) {
+        //TODO: check if the selected square can be moved to and, if so, move to it, and if not, select as normal
+        moving = false;
+    }
     //handles the context menu text which tells the player which grid square they have selected
     const currentlySelectedDefaultString = "Currently Selected Grid Space: ";
     const currentlySelectedMessage = document.getElementById('currently-selected-message');
@@ -46,12 +54,15 @@ const SelectGridSquare = (gridPosition) => {
     currentlyOccupyingMessage.innerHTML = currentlyOccupyingEmptyText;
     players.forEach((p) => {
         if (p.Position.xCoordinate === gridPosition.xCoordinate && p.Position.yCoordinate == gridPosition.yCoordinate) {
+            const buttonZone = document.getElementById("button-zone");
+            buttonZone.innerHTML = ``;
             if (p.PlayerName === playerStorage.getItem("Username")) {
                 currentlyOccupyingMessage.innerHTML = currentlyOccupyingDefaultText + "you";
+                buttonZone.innerHTML += `<button id="move-button" onclick="initiateMove()">Move</button>`;
             }
             else {
                 currentlyOccupyingMessage.innerHTML = currentlyOccupyingDefaultText + p.PlayerName;
-                //TODO: add button to send points
+                buttonZone.innerHTML += `<button id="send-point-button" onclick="SendActionPoint()">Send Action Point</button>`;
             }
         }
     });
@@ -61,9 +72,36 @@ canvas.addEventListener("mousedown", function (e) {
     const gridPosition = GetMouseGridPosition(canvas, e);
     SelectGridSquare(gridPosition);
 });
+//function to bring up the indicators to allow the player to move
+const initiateMove = () => {
+    moving = true;
+    const currentPlayer = players.filter((p) => p.PlayerName === playerStorage.getItem("Username"))[0];
+    //does 2 nested for loops to move around the player in all directions to show which spaces the player can move to
+    for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+            const currentSpace = {
+                xCoordinate: currentPlayer.Position.xCoordinate + i,
+                yCoordinate: currentPlayer.Position.yCoordinate + j
+            };
+            //makes sure that the space being worked on is a valid space
+            if ((currentSpace.xCoordinate > 0 && currentSpace.yCoordinate > 0) && (currentSpace.xCoordinate <= 38 && currentSpace.yCoordinate <= 18) && !(currentSpace.xCoordinate === currentPlayer.Position.xCoordinate && currentSpace.yCoordinate === currentPlayer.Position.yCoordinate)) {
+                //checks if the current space is empty by filtering the list of players down to only the players whose position matches the position of the current space
+                if (players.filter((p) => p.Position.xCoordinate === currentSpace.xCoordinate && p.Position.yCoordinate === currentSpace.yCoordinate).length === 0) {
+                    context.fillStyle = "orange";
+                    context.fillRect(((currentSpace.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize - indicatorMarkSize) / 2), ((currentSpace.yCoordinate - 1) * boardSquareSize) + ((boardSquareSize - indicatorMarkSize) / 2), indicatorMarkSize, indicatorMarkSize);
+                    console.log(currentSpace.xCoordinate + ", " + currentSpace.yCoordinate);
+                }
+            }
+        }
+    }
+};
+const SendActionPoint = () => {
+    //TODO: make it send an action point to the selected player 
+    console.log("Function not yet implemented");
+};
 //function to draw the board onto the canvas
 const drawBoard = () => {
-    //dras a black background to be the lines between the spaces
+    //draws a black background to be the lines between the spaces
     //sets the color to black
     context.fillStyle = "black";
     //draws the background
@@ -74,53 +112,55 @@ const drawBoard = () => {
             context.clearRect(i * boardSquareSize + 1, j * boardSquareSize + 1, boardSquareSize - 2, boardSquareSize - 2);
         }
     }
-    //draws all of the players in the game
-    //loops through the list of players and draws them at their position
-    players.forEach((p) => {
-        //sets the color of the players
-        context.fillStyle = "black";
-        //if it is drawing the player, set it to blue
-        if (p.PlayerName === playerStorage.getItem("Username")) {
-            context.fillStyle = "blue";
+    //variables for handling the importing of the player list
+    let playerListImport;
+    //get the list of players from the server and draws the board when the site loads
+    fetch(server + port + "/players", { method: "GET" })
+        .then(res => res.json())
+        //.then((players) => playerListImport = JSON.parse(players))
+        .then((players) => playerListImport = players)
+        .then(() => {
+        let i = 1;
+        while (playerListImport[i] != undefined) {
+            players.push(playerListImport[i]);
+            i++;
         }
-        context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize - playerSize) / 2), ((p.Position.yCoordinate - 1) * boardSquareSize) + ((boardSquareSize - playerSize) / 2), playerSize, playerSize);
-        //draws a pip for each health point the player has
-        //sets the color of the pips
-        context.fillStyle = "Red";
-        //draws the pip for 1 health
-        if (p.Health >= 1) {
-            context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize - playerSize) / 2), ((p.Position.yCoordinate - 1) * boardSquareSize) + (playerSize - 10), 10, 10);
-        }
-        //draws the pip for 2 health
-        if (p.Health >= 2) {
-            context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize - playerSize) / 2) + ((playerSize / 2) - 5), ((p.Position.yCoordinate - 1) * boardSquareSize) + (playerSize - 10), 10, 10);
-        }
-        //draws the pip for 3 health
-        if (p.Health >= 3) {
-            context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize - playerSize) / 2) + (playerSize - 10), ((p.Position.yCoordinate - 1) * boardSquareSize) + (playerSize - 10), 10, 10);
-        }
-        //fills in  the player info for the player
-        const availablePointsDefaultText = "Available Action Points: ";
-        if (p.PlayerName == playerStorage.getItem("Username")) {
-            const availablePointsDisplay = document.getElementById("points");
-            availablePointsDisplay.innerHTML = availablePointsDefaultText + p.Points;
-        }
-    });
+    })
+        .then(() => {
+        //draws all of the players in the game
+        //loops through the list of players and draws them at their position
+        players.forEach((p) => {
+            //sets the color of the players
+            context.fillStyle = "black";
+            //if it is drawing the player, set it to blue
+            if (p.PlayerName === playerStorage.getItem("Username")) {
+                context.fillStyle = "blue";
+            }
+            context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize - playerSize) / 2), ((p.Position.yCoordinate - 1) * boardSquareSize) + ((boardSquareSize - playerSize) / 2), playerSize, playerSize);
+            //draws a pip for each health point the player has
+            //sets the color of the pips
+            context.fillStyle = "Red";
+            //draws the pip for 1 health
+            if (p.Health >= 1) {
+                context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize - playerSize) / 2), ((p.Position.yCoordinate - 1) * boardSquareSize) + (playerSize - 10), 10, 10);
+            }
+            //draws the pip for 2 health
+            if (p.Health >= 2) {
+                context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize - playerSize) / 2) + ((playerSize / 2) - 5), ((p.Position.yCoordinate - 1) * boardSquareSize) + (playerSize - 10), 10, 10);
+            }
+            //draws the pip for 3 health
+            if (p.Health >= 3) {
+                context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize - playerSize) / 2) + (playerSize - 10), ((p.Position.yCoordinate - 1) * boardSquareSize) + (playerSize - 10), 10, 10);
+            }
+            //fills in  the player info for the player
+            const availablePointsDefaultText = "Available Action Points: ";
+            if (p.PlayerName == playerStorage.getItem("Username")) {
+                const availablePointsDisplay = document.getElementById("points");
+                availablePointsDisplay.innerHTML = availablePointsDefaultText + p.Points;
+            }
+        });
+    })
+        .catch(() => console.log("Error loading data from server"));
 };
-//variables for handling the importing of the player list
-let playerListImport;
-//get the list of players from the server and draws the board when the site loads
-fetch(server + port + "/players", { method: "GET" })
-    .then(res => res.json())
-    //.then((players) => playerListImport = JSON.parse(players))
-    .then((players) => playerListImport = players)
-    .then(() => {
-    let i = 1;
-    while (playerListImport[i] != undefined) {
-        players.push(playerListImport[i]);
-        i++;
-    }
-})
-    .then(drawBoard)
-    .catch(() => console.log("Error loading data from server"));
+drawBoard();
 //# sourceMappingURL=gameLogic.js.map
--- src/gameLogic.ts
+++ src/gameLogic.ts
@@ -14,8 +14,14 @@ const port: string = playerStorage.getItem("Port") as string
 const playerSize: number = 40
 //defines the size of the drawn board squares
 const boardSquareSize: number = 50
+//defines the size of the indicator marks
+const indicatorMarkSize: number = 30
 //holds a list of the players
 let players: Tank[] = [];
+//variables to hold the status of the client
+//bool to hold the status of if the player is currently trying to move
+let moving: boolean = false;
+
 //how far apart 2 tanks are for the purpose of checking if a tank is within
 //range to shoot
 const CheckRangeBetweenTanks = (tankA: Tank, tankB: Tank): number => Math.max(Math.abs(tankA.Position.xCoordinate - tankB.Position.xCoordinate), Math.abs(tankA.Position.yCoordinate - tankB.Position.yCoordinate))
@@ -36,8 +42,13 @@ const GetMouseGridPosition = (canvas: HTMLCanvasElement, event: any): Coordinate
 }
 //function to select a grid square
 const SelectGridSquare = (gridPosition: CoordinatePoint): void => {
-    //TODO: Detect if a player is in that position and open the appropriate
-    //context menu based on if it is their tank or another player's
+
+    //handles if the player is clicking on a space to try to move to it
+    if (moving) {
+        //TODO: check if the selected square can be moved to and, if so, move to it, and if not, select as normal
+        
+        moving = false;
+    }
 
     //handles the context menu text which tells the player which grid square they have selected
     const currentlySelectedDefaultString: string = "Currently Selected Grid Space: "
@@ -56,11 +67,14 @@ const SelectGridSquare = (gridPosition: CoordinatePoint): void => {
     currentlyOccupyingMessage.innerHTML = currentlyOccupyingEmptyText
     players.forEach((p): void => {
         if (p.Position.xCoordinate === gridPosition.xCoordinate && p.Position.yCoordinate == gridPosition.yCoordinate) {
+            const buttonZone: HTMLElement = document.getElementById("button-zone") as HTMLElement
+            buttonZone.innerHTML = ``
             if (p.PlayerName === playerStorage.getItem("Username")) {
                 currentlyOccupyingMessage.innerHTML = currentlyOccupyingDefaultText + "you"
+                buttonZone.innerHTML += `<button id="move-button" onclick="initiateMove()">Move</button>`
             } else {
                 currentlyOccupyingMessage.innerHTML = currentlyOccupyingDefaultText + p.PlayerName
-                //TODO: add button to send points
+                buttonZone.innerHTML += `<button id="send-point-button" onclick="SendActionPoint()">Send Action Point</button>`
             }
         }
     })
@@ -71,10 +85,39 @@ canvas.addEventListener("mousedown", function(e) {
     SelectGridSquare(gridPosition)
 })
 
+//function to bring up the indicators to allow the player to move
+const initiateMove = (): void => {
+    moving = true;
+    const currentPlayer: Tank = players.filter((p) => p.PlayerName === playerStorage.getItem("Username"))[0]
+    //does 2 nested for loops to move around the player in all directions to show which spaces the player can move to
+    for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+            const currentSpace: CoordinatePoint = {
+                xCoordinate: currentPlayer.Position.xCoordinate + i,
+                yCoordinate: currentPlayer.Position.yCoordinate + j
+            }
+            //makes sure that the space being worked on is a valid space
+            if ((currentSpace.xCoordinate > 0 && currentSpace.yCoordinate > 0) && (currentSpace.xCoordinate <= 38 && currentSpace.yCoordinate <= 18) && !(currentSpace.xCoordinate === currentPlayer.Position.xCoordinate && currentSpace.yCoordinate === currentPlayer.Position.yCoordinate)) {
+                //checks if the current space is empty by filtering the list of players down to only the players whose position matches the position of the current space
+                if (players.filter((p) => p.Position.xCoordinate === currentSpace.xCoordinate && p.Position.yCoordinate === currentSpace.yCoordinate).length === 0) {
+                    context.fillStyle = "orange"
+                    context.fillRect(((currentSpace.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize - indicatorMarkSize) / 2), ((currentSpace.yCoordinate - 1) * boardSquareSize) + ((boardSquareSize - indicatorMarkSize) / 2), indicatorMarkSize, indicatorMarkSize)
+                    console.log(currentSpace.xCoordinate + ", " + currentSpace.yCoordinate)
+                }
+            }
+        }
+    }
+}
+
+const SendActionPoint =  (): void => {
+    //TODO: make it send an action point to the selected player 
+    console.log("Function not yet implemented")
+}
+
 //function to draw the board onto the canvas
 const drawBoard = (): void => {
 
-    //dras a black background to be the lines between the spaces
+    //draws a black background to be the lines between the spaces
     //sets the color to black
     context.fillStyle = "black"
     //draws the background
@@ -86,54 +129,56 @@ const drawBoard = (): void => {
         }
     }
 
-    //draws all of the players in the game
-    //loops through the list of players and draws them at their position
-    players.forEach((p) => {
-        //sets the color of the players
-        context.fillStyle = "black"
-        //if it is drawing the player, set it to blue
-        if (p.PlayerName === playerStorage.getItem("Username")) {
-            context.fillStyle = "blue"
-        }
-        context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize-playerSize) / 2), ((p.Position.yCoordinate-1) * boardSquareSize) + ((boardSquareSize-playerSize) / 2),  playerSize, playerSize)
-        //draws a pip for each health point the player has
-        //sets the color of the pips
-        context.fillStyle = "Red"
-        //draws the pip for 1 health
-        if (p.Health >= 1) {
-            context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize-playerSize) / 2), ((p.Position.yCoordinate-1) * boardSquareSize) + (playerSize - 10), 10, 10)
-        }
-        //draws the pip for 2 health
-        if (p.Health >= 2) {
-            context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize-playerSize) / 2) + ((playerSize / 2) - 5), ((p.Position.yCoordinate-1) * boardSquareSize) + (playerSize - 10), 10, 10)
-        }
-        //draws the pip for 3 health
-        if (p.Health >= 3) {
-            context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize-playerSize) / 2) + (playerSize - 10), ((p.Position.yCoordinate-1) * boardSquareSize) + (playerSize - 10), 10, 10)
-        }
-
-        //fills in  the player info for the player
-        const availablePointsDefaultText: string = "Available Action Points: "
-        if (p.PlayerName == playerStorage.getItem("Username")) {
-            const availablePointsDisplay: HTMLElement = document.getElementById("points") as HTMLElement
-            availablePointsDisplay.innerHTML = availablePointsDefaultText + p.Points
-        }
-    })
+    //variables for handling the importing of the player list
+    let playerListImport: any
+    //get the list of players from the server and draws the board when the site loads
+    fetch(server + port + "/players", {method: "GET"})
+       .then(res => res.json())
+       //.then((players) => playerListImport = JSON.parse(players))
+       .then((players) => playerListImport = players)
+       .then(() => {
+            let i = 1
+            while (playerListImport[i] != undefined) {
+                players.push(playerListImport[i] as Tank)
+                i++
+            }
+       })
+       .then(() => {
+            //draws all of the players in the game
+            //loops through the list of players and draws them at their position
+            players.forEach((p) => {
+                //sets the color of the players
+                context.fillStyle = "black"
+                //if it is drawing the player, set it to blue
+                if (p.PlayerName === playerStorage.getItem("Username")) {
+                    context.fillStyle = "blue"
+                }
+                context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize-playerSize) / 2), ((p.Position.yCoordinate-1) * boardSquareSize) + ((boardSquareSize-playerSize) / 2),  playerSize, playerSize)
+                //draws a pip for each health point the player has
+                //sets the color of the pips
+                context.fillStyle = "Red"
+                //draws the pip for 1 health
+                if (p.Health >= 1) {
+                    context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize-playerSize) / 2), ((p.Position.yCoordinate-1) * boardSquareSize) + (playerSize - 10), 10, 10)
+                }
+                //draws the pip for 2 health
+                if (p.Health >= 2) {
+                    context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize-playerSize) / 2) + ((playerSize / 2) - 5), ((p.Position.yCoordinate-1) * boardSquareSize) + (playerSize - 10), 10, 10)
+                }
+                //draws the pip for 3 health
+                if (p.Health >= 3) {
+                    context.fillRect(((p.Position.xCoordinate - 1) * boardSquareSize) + ((boardSquareSize-playerSize) / 2) + (playerSize - 10), ((p.Position.yCoordinate-1) * boardSquareSize) + (playerSize - 10), 10, 10)
+                }
+            
+                //fills in  the player info for the player
+                const availablePointsDefaultText: string = "Available Action Points: "
+                if (p.PlayerName == playerStorage.getItem("Username")) {
+                    const availablePointsDisplay: HTMLElement = document.getElementById("points") as HTMLElement
+                    availablePointsDisplay.innerHTML = availablePointsDefaultText + p.Points
+                }
+            })
+       })
+       .catch(() => console.log("Error loading data from server"))
 }
 
-//variables for handling the importing of the player list
-let playerListImport: any
-//get the list of players from the server and draws the board when the site loads
-fetch(server + port + "/players", {method: "GET"})
-   .then(res => res.json())
-   //.then((players) => playerListImport = JSON.parse(players))
-   .then((players) => playerListImport = players)
-   .then(() => {
-        let i = 1
-        while (playerListImport[i] != undefined) {
-            players.push(playerListImport[i] as Tank)
-            i++
-        }
-   })
-   .then(drawBoard)
-   .catch(() => console.log("Error loading data from server"))
+drawBoard()
